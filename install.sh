be2e6538e45903b2f6b895485a59bc0044e10477
Will Faurot <will.faurot@instacart.com>
2017-01-16 15:36:13 -0800

    first pass at install script

--- install.sh
+++ install.sh
@@ -1,12 +1,64 @@
-# if homebrew isn't installed
-#   install homebrew
-#   brew update
-#   install some programz (zsh, ??)
+# Install utils without having to install xcode
+# xcode-select --install
 
-# pull dotfiles
+bootstrap() {
+  # install homebrew
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# how to do atom config?
+  brew update
 
-# do symlinks
-# how to loop through with a blob?
+  brew install git
+  brew install tig
+  brew install tmux
+  brew install the_silver_searcher
+  brew install rbenv # ruby
+  brew install opam  # ocaml
 
+  brew install vim --with-override-system-vi
+
+  brew cleanup
+
+  # nvm
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | zsh
+  echo 'Checking nvm installation...'
+  command -v nvm
+
+  # elm
+  npm install -g elm
+
+  # gvm. to install:
+  # gvm listall
+  # gvm install go<whatever the latest version was>
+  # gvm use go1.4 --default
+  # zsh < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+
+  # vundle
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  vim +PluginInstall +qall
+}
+
+install_dotfiles() {
+  git pull origin master
+
+  rsync --exclude ".git/" \
+    --exclude "install.sh" \
+    --exclude "README.md" \
+    -avh --no-perms . ~
+
+  source ~/.zshrc
+}
+
+case "$1" in
+  bootstrap)
+    bootstrap
+    ;;
+  dotfiles)
+    install_dotfiles
+    ;;
+  *)
+    bootstrap
+    install_doftiles
+    # bootstrap
+    # dotfiles
+    ;;
+esac
